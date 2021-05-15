b17a0d3e2a037c245cb62496e859612ac6c43edd
betopinheiro1005 <betopinheiro1005@yahoo.com.br>
2021-05-15 14:42:36 -0300

    First commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://betopinheiro1005:angstron1005@node-store-cluster.l4jj0.mongodb.net/node-store-db?retryWrites=true&w=majority',
+    sendgridKey: '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',
+    containerConnectionString: 'TBD'
+}
